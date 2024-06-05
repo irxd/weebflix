44963dfd6a356547ab24eafa18a708ecc207ce40
irxd <irsyart@gmail.com>
2024-06-05 07:29:54 +0700

    chore: set unoptimized image

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,11 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'cdn.myanimelist.net',
+        protocol: "https",
+        hostname: "cdn.myanimelist.net",
       },
     ],
+    unoptimized: true,
   },
 };
 
